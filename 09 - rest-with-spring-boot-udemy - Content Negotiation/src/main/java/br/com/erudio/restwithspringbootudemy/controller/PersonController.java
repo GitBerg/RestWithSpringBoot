c4aff66fcd02c8ad34d5a808b9050adbc15726c8
Gutemberg S Filho <beguinhoboy@hotmail.com>
2021-04-02 12:36:06 -0300

    Content Negotiation via Extensão, QUERY Param e Header

---
+++ 09 - rest-with-spring-boot-udemy - Content Negotiation/src/main/java/br/com/erudio/restwithspringbootudemy/controller/PersonController.java
@@ -0,0 +1,53 @@
+package br.com.erudio.restwithspringbootudemy.controller;
+
+import br.com.erudio.restwithspringbootudemy.data.vo.v1.PersonVO;
+import br.com.erudio.restwithspringbootudemy.services.personServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/person/v1")
+public class PersonController {
+
+    @Autowired
+    private personServices services;
+
+
+    @GetMapping(produces = { "application/json", "application/xml"})
+    public List<PersonVO> findAll() {
+
+        return services.findAll();
+    }
+
+    @GetMapping(value = "/{id}", produces = { "application/json", "application/xml"})
+    public PersonVO findById(@PathVariable("id") Long id) {
+
+        return services.findById(id);
+    }
+
+    @PostMapping(produces = {"application/json", "application/xml"},
+    consumes = {"application/json", "application/xml"})
+    public PersonVO create(@RequestBody PersonVO person) {
+
+        return services.create(person);
+    }
+
+    @PutMapping(produces = {"application/json", "application/xml"},
+            consumes = {"application/json", "application/xml"})
+    public PersonVO update(@RequestBody PersonVO person) {
+
+        return services.update(person);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+         services.delete(id);
+         return ResponseEntity.ok().build();
+    }
+
+
+
+}
