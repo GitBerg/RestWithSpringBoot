a6c6dc56640ba6bca362ba2e4cc441f3e43e15f4
Gutemberg S Filho <beguinhoboy@hotmail.com>
2021-04-02 14:59:17 -0300

    HATEOAS com erros

---
+++ 10 - rest-with-spring-boot-udemy - HATEOAS/src/main/java/br/com/erudio/restwithspringbootudemy/services/personServices.java
@@ -0,0 +1,58 @@
+package br.com.erudio.restwithspringbootudemy.services;
+
+import java.util.List;
+
+import br.com.erudio.restwithspringbootudemy.converter.DozerConverter;
+import br.com.erudio.restwithspringbootudemy.data.model.Person;
+import br.com.erudio.restwithspringbootudemy.data.vo.v1.PersonVO;
+import br.com.erudio.restwithspringbootudemy.exception.ResourceNotFoundException;
+import br.com.erudio.restwithspringbootudemy.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class personServices {
+
+    @Autowired
+    PersonRepository repository;
+
+    public PersonVO create(PersonVO person) {
+        var entity = DozerConverter.parseObject(person, Person.class);
+        var vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+        return vo;
+    }
+
+    public List<PersonVO> findAll() {
+        return DozerConverter.parseListObjects(repository.findAll(), PersonVO.class);
+    }
+
+    public PersonVO findById(Long id) {
+
+        var entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+        return DozerConverter.parseObject(entity, PersonVO.class);
+    }
+
+    public PersonVO update(PersonVO person) {
+        var entity = repository.findById(person.getKey())
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAndress(person.getAndress());
+        entity.setGender(person.getGender());
+
+        var vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+        return vo;
+    }
+
+    public void delete(Long id) {
+        Person entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+        repository.delete(entity);
+    }
+
+}
+
