204113ef82942e4ca2ae2a88f56f26f2a6b3b30c
Gutemberg S Filho <beguinhoboy@hotmail.com>
2021-04-04 17:32:33 -0300

    Adcionando suporte a CORS na API

---
+++ 12 - rest-with-spring-boot-udemy - CORS/src/main/java/br/com/erudio/restwithspringbootudemy/config/WebConfig.java
@@ -0,0 +1,62 @@
+package br.com.erudio.restwithspringbootudemy.config;
+
+import br.com.erudio.restwithspringbootudemy.serialization.converter.YamlJackson2HttpMessageConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private static final MediaType MEDIA_TYPE_YML = MediaType.valueOf("application/x-yaml");
+
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters){
+        converters.add(new YamlJackson2HttpMessageConverter());
+    }
+
+    public void addCorsMappings(CorsRegistry regs){
+        regs.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH", "HEAD","TRACE","CONNECT","OPTIONS");
+    }
+
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer ) {
+
+
+        //Extension
+
+      /*  configurer.favorParameter(false).ignoreAcceptHeader(false)
+                .defaultContentType(MediaType.APPLICATION_JSON).mediaType("json",
+                MediaType.APPLICATION_JSON).mediaType("xml", MediaType.APPLICATION_XML);
+    }*/
+
+        //QUERY Parameter
+
+             /*   configurer.favorPathExtension(false).favorParameter(true).parameterName("mediaType").ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false)
+                        .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);*/
+
+
+        //Header
+
+        configurer.favorParameter(false).ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("x-yaml", MEDIA_TYPE_YML);
+          }
+
+
+
+
+}
---
+++ 12 - rest-with-spring-boot-udemy - CORS/src/main/java/br/com/erudio/restwithspringbootudemy/data/vo/v1/PersonVO.java
@@ -0,0 +1,90 @@
+package br.com.erudio.restwithspringbootudemy.data.vo.v1;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+
+@JsonPropertyOrder(value = {"Id", "First_Name","Last_Name","Address" })
+public class PersonVO extends RepresentationModel<PersonVO> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
+    @JsonProperty(value="Id")
+    private long id;
+    @JsonProperty(value="First_Name")
+    private String firstName;
+    @JsonProperty(value="Last_Name")
+    private String lastName;
+    @JsonProperty(value = "Address")
+    private String andress;
+    @JsonIgnore
+    private String gender;
+
+    public PersonVO() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAndress() {
+        return andress;
+    }
+
+    public void setAndress(String andress) {
+        this.andress = andress;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PersonVO personVO = (PersonVO) o;
+
+        return new EqualsBuilder().append(id, personVO.id).append(firstName, personVO.firstName).append(lastName, personVO.lastName).append(andress, personVO.andress).append(gender, personVO.gender).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37).append(id).append(firstName).append(lastName).append(andress).append(gender).toHashCode();
+    }
+}
+
+
